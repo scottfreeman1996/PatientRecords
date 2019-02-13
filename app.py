9d7abba0a5a2cdc0908e65e865b20e27f08b6d93
scottfreeman1996 <Scott61079@Mastek.Com>
2019-02-13 14:34:10 +0000

    find patient by id

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, Response
 import json
-from settings import *
+
 
 #so flask knows where to look for stuff
 #__name__ returns module name for this file
--- scott.py
+++ scott.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request, Response
 import json
 from flask_sqlalchemy import SQLAlchemy
+import jsonpickle
+from mysql import connector
 
 
 app = Flask(__name__)
@@ -24,8 +26,11 @@ class Patient(db.Model):
         self.area = params["area"]
         self.phone_no = params["phone_no"]
         pass
+    
+    def __str__(self):
+        return"Id:"+ str(self.patient_id)+" Name:"+self.name+" Gender:"+self.gender+" Date of Birth:"+self.date_of_birth+" Area:"+self.area+" Phone Number:"+str(self.phone_no)
 
-@app.route('/patients')
+@app.route('/patients/register',methods=['POST'])
 def create_Patient():
     p = Patient({"name":"Test Patient","gender":"male","date_of_birth":"01/01/2010",
                 "area":"Leeds","phone_no":67})
@@ -39,11 +44,49 @@ def create_Patient():
         print("ID:",p.patient_id,"Name:",p.name,"Gender:",p.gender, "Date of Birth:",p.date_of_birth,
             "Area:",p.area, "Phone Number:",p.phone_no)
 
+    pass
+    
+    
+
+
+@app.route('/patients/list')
+def get_Patients():
+    
+    patient_list = db.session.query(Patient).all()
+    display_patient_list = []
+    for patient in patient_list:
+        display_patient_list.append({"patient_id":patient.patient_id,"name":patient.name,"gender":patient.gender,
+        "date_of_birth":patient.date_of_birth,"area":patient.area,"phone_no":patient.phone_no})
+    return jsonpickle.encode(display_patient_list)
+
+
+
+@app.route('/patients/<int:patient_id>')
+def get_patient_by_patient_id(patient_id):
+
+    patient_list = db.session.query(Patient).filter_by(patient_id=int(patient_id)).all()
+    display_patient_list = []
+
+    for patient in patient_list:
+        display_patient_list.append({"patient_id":patient.patient_id,"name":patient.name,"gender":patient.gender,
+        "date_of_birth":patient.date_of_birth,"area":patient.area,"phone_no":patient.phone_no})
+    return jsonpickle.encode(display_patient_list)
+
+
+@app.route('/patients/<int:patient_id>', methods = ['DELETE'])
+def delete_patient(patient_id):
+    patient_list = db.session.query(Patient).filter_by(patient_id=int(patient_id)).all()
+
+  
+    
+
+
 
 
 if __name__ == '__main__':
     #db.create_all()
-
-    create_Patient()
+    #get_patient_by_patient_id(1)
+    #get_Patients()
+    #create_Patient()
     app.run(port=5000)
     pass
