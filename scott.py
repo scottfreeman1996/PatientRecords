b014983d82c10243b0b5b537bf5a95520bdcb2e3
scottfreeman1996 <Scott61079@Mastek.Com>
2019-02-14 09:45:44 +0000

    merge

--- scott.py
+++ scott.py
@@ -3,18 +3,16 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 import jsonpickle
 from mysql import connector
-
+from flask.templating import render_template
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/patient_records'
 db = SQLAlchemy(app)
 
-report_chart = db.Table('report_chart',
-            db.Column('patient_id', db.Integer, db.ForeignKey('patient_details.patient_id'),
-                primary_key=True),
-            db.Column('report_id', db.Integer, db.ForeignKey('reports.report_id'),
-                primary_key=True))
+#classes
+
 
 class Patient(db.Model):
     __tablename__= "patient_details"
@@ -24,7 +22,7 @@ class Patient(db.Model):
     date_of_birth = db.Column("date_of_birth", db.String(10))
     area = db.Column("area", db.String(20))
     phone_no = db.Column("phone_no", db.Integer)
-    reports = db.relationship('Report',backref='patient',lazy=True)
+    #reports = db.relationship('Report',backref='patient',lazy=True)
     
     def __init__(self,params):
         self.name = params["name"]
@@ -37,11 +35,27 @@ class Patient(db.Model):
     def __str__(self):
         return"Id:"+ str(self.patient_id)+" Name:"+self.name+" Gender:"+self.gender+" Date of Birth:"+self.date_of_birth+" Area:"+self.area+" Phone Number:"+str(self.phone_no)
 
+class Report(db.Model):
+    __tablename__ = "reports"
+    report_id = db.Column(db.Integer,primary_key=True)
+    symptoms = db.Column(db.String(1000))
+    date = db.Column(db.String(10))
+    diagnosis = db.Column(db.String(50))
+    patient_id = db.Column(db.Integer)#, db.ForeignKey('patient_id'),nullable=False)
+
+db.create_all()
+
+@app.route('/')
+def application_home():
+    return 'Welcome to NHS Application'
+
+#Patient class http methods
+
 @app.route('/patients/register',methods=['POST'])
 def create_Patient():
     p = Patient({"name":"Test Patient","gender":"male","date_of_birth":"01/01/2010",
                 "area":"Leeds","phone_no":67})
-    
+
     db.session.add(p)
     db.session.commit()
 
@@ -87,13 +101,64 @@ def delete_patient(patient_id):
     db.session.commit()
     return jsonpickle.encode(patient)
 
+#Report class http methods
+
+@app.route("/add-report/<int:patient_id>",methods=['POST'])
+def add_report(patient_id):
+    request_data = request.get_json()
+    request_data["patient_id"] = int(patient_id)
+    r = Report(**request_data)
+    
+    p.reports.append(r)
+
+    db.session.add(r)
+    db.session.commit()
+    return jsonify(request_data)
+
+@app.route("/get-reports/<int:patient_id>")
+def get_reports(patient_id):
+    report_list = db.session.query(Report).filter_by(patient_id=int(patient_id)).all()
+    display_report_list = []
+    for report in report_list:
+        display_report_list.append({"report_id":report.report_id,"symptoms":report.symptoms,"date":report.date,"diagnosis":report.diagnosis,"patient_id":report.patient_id})
+    return jsonpickle.encode(display_report_list)
+
+@app.route("/delete-report/<int:report_id>",methods=['DELETE'])
+def delete_report(report_id):
+    report = Report.query.get(int(report_id))
+    db.session.delete(report)
+    db.session.commit()
+    return_report = {"report_id":report.report_id,"symptoms":report.symptoms,"date":report.date,"diagnosis":report.diagnosis,"patient_id":report.patient_id}
+    return jsonpickle.encode(return_report)
+
+@app.route("/update-report/<int:report_id>",methods=['POST'])
+def edit_report(report_id):
+    request_data = request.get_json()
+    report = Report.query.get(int(report_id))
+    report.symptoms = request_data["symptoms"]
+    report.date = request_data["date"]
+    report.diagnosis = request_data["diagnosis"]
+    report.patient_id = request_data["patient_id"]
+    db.session.commit()
+    return_report = {"report_id":report.report_id,"symptoms":report.symptoms,"date":report.date,"diagnosis":report.diagnosis,"patient_id":report.patient_id}
+    return jsonpickle.encode(return_report)
 
 
+@app.route('/web/patients/register', methods=["POST"])
+def register_patient_web():
+    create_Patient(
+        Employee({"patient_id":int(request.form.get("patient_id")),
+                  "name":request.form.get("name"),
+                  "gender":request.form.get("gender"),
+                  "date_of_birth":request.form.get("date_of_birth"),
+                  "area":request.form.get("area"),
+                  "phone_no":request.form.get("phone_no"),}))
+    return redirect("/web/patients") # redirects user to patient list
 
-if __name__ == '__main__':
-    #db.create_all()
-    #get_patient_by_patient_id(1)
-    #get_Patients()
-    #create_Patient()
-    app.run(port=5000)
-    pass
+@app.route('/web/patients')
+def display_patient_page():
+    return render_template("patient.html", result=get_Patients(), 
+                           content_type="application/json")
+    
+
+app.run(port=5000)
